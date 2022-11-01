c61c1c11aca70e4ad2ffb777216c1ac94b1fc270
Saerohoon Yang <saerohoon.yang@meshkorea.net>
2022-11-02 00:31:20 +0900

    - JPQL로 query 사용해 JpaRepository가 지원하지 않는 query 구현 - 페이징 처리

--- chapter02_2/src/main/java/dev/yangsae/chapter02_2/Chapter022Application.java
+++ chapter02_2/src/main/java/dev/yangsae/chapter02_2/Chapter022Application.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
 public class Chapter022Application implements CommandLineRunner {
@@ -21,9 +24,20 @@ public class Chapter022Application implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Customer created =
                 customerRepository.save(
-                        new Customer(null, "Saerohoon", "Yang"));
+                        new Customer(null, "Saerohoon1", "Yang"));
         System.out.println("created = " + created);
 
-        customerRepository.findAll().forEach(System.out::println);
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<Customer> page = customerRepository.findAll(pageable);
+
+        System.out.println("한 페이지당 데이터 수=" + page.getSize());
+        System.out.println("현재 페이지=" + page.getNumber());
+        System.out.println("전체 페이지 수=" + page.getTotalPages());
+        System.out.println("전체 데이터 수=" + page.getTotalElements());
+
+        page.get().forEach(o -> {
+                    System.out.printf("customer : %s %s", o.getFirstName(), o.getLastName());
+                    System.out.println();
+                });
     }
 }
--- chapter02_2/src/main/java/dev/yangsae/chapter02_2/repository/CustomerRepository.java
+++ chapter02_2/src/main/java/dev/yangsae/chapter02_2/repository/CustomerRepository.java
@@ -2,10 +2,14 @@ package dev.yangsae.chapter02_2.repository;
 
 import dev.yangsae.chapter02_2.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-
+    @Query("SELECT c FROM Customer c ORDER BY c.firstName, c.lastName")
+    List<Customer> findAllOrderByName();
 }
