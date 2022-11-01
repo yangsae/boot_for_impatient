3b07ebc7d44cca03220c13e63d28a23b2752bbad
Saerohoon Yang <saerohoon.yang@meshkorea.net>
2022-11-01 22:05:12 +0900

    Frontend 대신 CommandLineRunner 이용

--- chapter02/src/main/java/dev/yangsae/chapter02/Chapter02Application.java
+++ chapter02/src/main/java/dev/yangsae/chapter02/Chapter02Application.java
@@ -1,5 +1,7 @@
 package dev.yangsae.chapter02;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,17 +9,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 import java.io.IOException;
 
 @SpringBootApplication
-public class Chapter02Application {
+public class Chapter02Application implements CommandLineRunner {
+
+    @Autowired
+    ArgumentResolver argumentResolver;
+    @Autowired
+    Calculator calculator;
 
     public static void main(String[] args) {
-        try(ConfigurableApplicationContext context =
-                    SpringApplication.run(Chapter02Application.class, args)) {
-
-            Frontend frontend = context.getBean(Frontend.class);
-            frontend.run();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SpringApplication.run(Chapter02Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Enter 2 numbers like 'a b' : ");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.a(), argument.b());
+        System.out.println("result = " + result);
+    }
 }
