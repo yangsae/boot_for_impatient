3198b1b8b1bfef99a55d0f5db5025b1dcc933e6d
Saerohoon Yang <saerohoon.yang@meshkorea.net>
2022-11-01 17:47:44 +0900

    chapter 2 initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# boot_for_impatient
-Spring boot for the really impatient 책 정리
+# 가장 빨리 만나는 스프링 부트
+- 기초 복습 차원에서 Spring boot for the really impatient 책을 정리합니다.
---
+++ chapter02/src/test/java/dev/yangsae/chapter02/Chapter02ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yangsae.chapter02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
